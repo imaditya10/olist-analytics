4a78136497e4b3aa0227af062291f4541601ecf7
imaditya10 <140335713+imaditya10@users.noreply.github.com>
2026-01-24 14:41:29 +0530

    Create 06_analysis.sql

---
+++ sql/06_analysis.sql
@@ -0,0 +1,123 @@
+
+-- BUSINESS ANALYSIS
+
+-- Purpose:
+-- Generate business insights from fact and dimension tables.
+
+
+
+-- ---------------------
+-- 1. TOTAL ORDERS & CUSTOMERS
+-- ---------------------
+SELECT
+    COUNT(DISTINCT o.order_id) AS total_orders,
+    COUNT(DISTINCT o.customer_id) AS total_customers
+FROM fact_orders o;
+
+
+-- ---------------------
+-- 2. MONTHLY ORDER TREND
+-- ---------------------
+SELECT
+    DATE_TRUNC('month', o.order_purchase_ts) AS order_month,
+    COUNT(*) AS total_orders
+FROM fact_orders o
+GROUP BY 1
+ORDER BY 1;
+
+
+-- ---------------------
+-- 3. TOTAL REVENUE
+-- ---------------------
+SELECT
+    SUM(p.payment_value) AS total_revenue
+FROM fact_payments p;
+
+
+-- ---------------------
+-- 4. MONTHLY REVENUE TREND
+-- ---------------------
+SELECT
+    DATE_TRUNC('month', o.order_purchase_ts) AS revenue_month,
+    SUM(p.payment_value) AS monthly_revenue
+FROM fact_orders o
+JOIN fact_payments p
+    ON o.order_id = p.order_id
+GROUP BY 1
+ORDER BY 1;
+
+
+-- ---------------------
+-- 5. TOP 10 PRODUCTS BY REVENUE
+-- ---------------------
+SELECT
+    dp.product_category_english,
+    SUM(oi.price) AS total_revenue
+FROM fact_order_items oi
+JOIN dim_products dp
+    ON oi.product_id = dp.product_id
+GROUP BY 1
+ORDER BY total_revenue DESC
+LIMIT 10;
+
+
+-- ---------------------
+-- 6. TOP 10 SELLERS BY REVENUE
+-- ---------------------
+SELECT
+    ds.seller_id,
+    SUM(oi.price) AS total_revenue
+FROM fact_order_items oi
+JOIN dim_sellers ds
+    ON oi.seller_id = ds.seller_id
+GROUP BY 1
+ORDER BY total_revenue DESC
+LIMIT 10;
+
+
+-- ---------------------
+-- 7. AVERAGE ORDER VALUE (AOV)
+-- ---------------------
+SELECT
+    ROUND(SUM(p.payment_value) / COUNT(DISTINCT p.order_id), 2) AS avg_order_value
+FROM fact_payments p;
+
+
+-- ---------------------
+-- 8. DELIVERY PERFORMANCE (ON-TIME VS LATE)
+-- ---------------------
+SELECT
+    CASE
+        WHEN o.order_delivered_ts <= o.estimated_delivery_date
+        THEN 'On Time'
+        ELSE 'Late'
+    END AS delivery_status,
+    COUNT(*) AS total_orders
+FROM fact_orders o
+WHERE o.order_delivered_ts IS NOT NULL
+GROUP BY 1;
+
+
+-- ---------------------
+-- 9. CUSTOMER ORDER DISTRIBUTION
+-- ---------------------
+SELECT
+    order_count,
+    COUNT(*) AS customers
+FROM (
+    SELECT
+        customer_id,
+        COUNT(order_id) AS order_count
+    FROM fact_orders
+    GROUP BY customer_id
+) t
+GROUP BY order_count
+ORDER BY order_count;
+
+
+-- ---------------------
+-- 10. AVERAGE REVIEW SCORE
+-- ---------------------
+SELECT
+    ROUND(AVG(r.review_score), 2) AS avg_review_score
+FROM stg_reviews r;
